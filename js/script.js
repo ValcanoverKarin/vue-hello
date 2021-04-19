42cf7a15e7462530dd1563acaa907377305a419f
karin valcanover <valcanoverkarin@outlook.it>
2021-04-19 16:21:30 +0200

    dichiaro dentro quale conteiner sarà utilizzato vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 //1. Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+//1a. dichiaro la variabile var perche deve essere globale e dichiaro che sto usando Vue
+var app = new Vue (
+    //1b. oggetto dove andrò a scrivere quali caratteristiche, funzionalità..... l applicazione deve avere
+    {
+        //1c. el dice a Vue su quale elemento html lavorare
+        el: '#root',
+        data: {
+
+        }
+    }
+)
